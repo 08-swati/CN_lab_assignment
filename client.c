c0fcda7994157a215dcfbe032f9120001fe8d5e3
08-swati <08swatisah@gmail.com>
2026-09-25 07:23:48 -0700

    lab04

--- client.c
+++ client.c
@@ -4,49 +4,46 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define PORT 8081
+#define PORT 8080
 #define BUFFER_SIZE 1024
 
 int main() {
-    int sock;
+    int sock = 0;
     struct sockaddr_in serv_addr;
-    char buffer[BUFFER_SIZE];
-    char message[BUFFER_SIZE];
 
-    // Create socket
+    char reg_no[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE];
+
     sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (sock < 0) {
-        perror("Socket creation failed");
-        exit(1);
-    }
-
-    // Configure server address
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+    inet_pton(AF_INET, "127.0.0.1",
+              &serv_addr.sin_addr);
 
-    // Connect to server
-    if (connect(sock, (struct sockaddr *) &serv_addr,
-                sizeof(serv_addr)) < 0) {
-        perror("Connection failed");
-        exit(1);
-    }
-
-    printf("Connected to server.\n");
+    connect(sock, (struct sockaddr *)&serv_addr,
+            sizeof(serv_addr));
 
     while (1) {
-        printf("Client: ");
-        scanf("%s", message);
+        printf("\nEnter Registration Number (or type 'bye' to exit): ");
 
-        send(sock, message, strlen(message), 0);
+        fgets(reg_no, BUFFER_SIZE, stdin);
 
-        if (strcmp(message, "bye") == 0)
+        reg_no[strcspn(reg_no, "\n")] = 0;
+
+        send(sock, reg_no, strlen(reg_no), 0);
+
+        if (strcmp(reg_no, "bye") == 0) {
+            printf("Connection closed.\n");
             break;
+        }
 
-        recv(sock, buffer, BUFFER_SIZE, 0);
-        printf("Server: %s\n", buffer);
+        memset(buffer, 0, BUFFER_SIZE);
+
+        read(sock, buffer, BUFFER_SIZE);
+
+        printf("Server Response: %s\n", buffer);
     }
 
     close(sock);
--- server.c
+++ server.c
@@ -4,67 +4,79 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define PORT 8081
+#define PORT 8080
 #define BUFFER_SIZE 1024
+
+void find_student(char *reg_no, char *response) {
+    FILE *fp = fopen("students.txt", "r");
+
+    if (fp == NULL) {
+        strcpy(response, "Error opening file");
+        return;
+    }
+
+    char file_reg[50], name[50], branch[50], college[100];
+    int found = 0;
+
+    while (fscanf(fp, "%s %s %s %s",
+                  file_reg, name, branch, college) != EOF) {
+
+        if (strcmp(file_reg, reg_no) == 0) {
+            sprintf(response,
+                    "Name: %s, Branch: %s, College: %s",
+                    name, branch, college);
+
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        strcpy(response, "Data not found");
+    }
+
+    fclose(fp);
+}
 
 int main() {
     int server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE];
-    char message[BUFFER_SIZE];
 
-    // Create socket
+    char buffer[BUFFER_SIZE];
+    char response[BUFFER_SIZE];
+
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (server_fd < 0) {
-        perror("Socket creation failed");
-        exit(1);
-    }
-
-    // Configure address
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // Bind socket
-    if (bind(server_fd, (struct sockaddr *) &address,
-             sizeof(address)) < 0) {
-        perror("Bind failed");
-        exit(1);
-    }
+    bind(server_fd, (struct sockaddr *)&address,
+         sizeof(address));
 
-    // Listen for connections
-    if (listen(server_fd, 5) < 0) {
-        perror("Listen failed");
-        exit(1);
-    }
+    listen(server_fd, 5);
 
-    printf("Server is listening on port %d...\n", PORT);
+    printf("Server is running...\n");
 
-    // Accept connection
-    new_socket = accept(server_fd, (struct sockaddr *) &address,
-                        (socklen_t *) &addrlen);
+    new_socket = accept(server_fd,
+                        (struct sockaddr *)&address,
+                        (socklen_t *)&addrlen);
 
-    if (new_socket < 0) {
-        perror("Accept failed");
-        exit(1);
-    }
-
-    printf("Client connected.\n");
-
-    // Communication loop
     while (1) {
-        recv(new_socket, buffer, BUFFER_SIZE, 0);
-        printf("Client: %s\n", buffer);
+        memset(buffer, 0, BUFFER_SIZE);
 
-        if (strcmp(buffer, "bye") == 0)
+        read(new_socket, buffer, BUFFER_SIZE);
+
+        if (strcmp(buffer, "bye") == 0) {
+            printf("Client disconnected.\n");
             break;
+        }
 
-        printf("Server: ");
-        scanf("%s", message);
+        find_student(buffer, response);
 
-        send(new_socket, message, strlen(message), 0);
+        send(new_socket, response,
+             strlen(response), 0);
     }
 
     close(new_socket);
